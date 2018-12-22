c628d8ecb6d96fe1e214cdf99f4012c6f8c5789c
Scottie Enriquez <scott.c.enriquez@gmail.com>
2018-12-22 14:35:16 -0600

    Added FontAwesome icons to MenuNavbar component; added collapse functionality to MenuNavbar component

--- scottie-gg-xxx/src/components/MenuNavbar.js
+++ scottie-gg-xxx/src/components/MenuNavbar.js
@@ -1,34 +1,47 @@
 import React, {Component} from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import logo from '../assets/graphics/full-name-logo.svg';
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+import { faKeyboard } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class MenuNavbar extends Component {
 
     render() {
         return (
-            <Navbar inverse fixedTop={true}>
+            <Navbar inverse fixedTop collapseOnSelect fluid={true}>
                 <Navbar.Header>
                     <Navbar.Brand>
                         <a href="/"><img src={logo} alt="logo" height={20}/></a>
                     </Navbar.Brand>
+                    <Navbar.Toggle />
                 </Navbar.Header>
-                <Nav>
-                    <NavItem className="ahamono to-uppercase" href="/">
-                        Resume
-                    </NavItem>
-                    <NavItem className="ahamono to-uppercase" href="/">
-                        Code
-                    </NavItem>
-                    <NavItem className="ahamono to-uppercase" href="/">
-                        Writing
-                    </NavItem>
-                    <NavItem className="ahamono to-uppercase" href="/">
-                        Gaming
-                    </NavItem>
-                    <NavItem className="ahamono to-uppercase" href="/">
-                        Meta
-                    </NavItem>
-                </Nav>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem className="ahamono to-uppercase" href="/">
+                            <FontAwesomeIcon icon={faFilePdf} /> Resume
+                        </NavItem>
+                        <NavItem className="ahamono to-uppercase" href="/">
+                            <FontAwesomeIcon icon={faCodeBranch} /> Code
+                        </NavItem>
+                        <NavItem className="ahamono to-uppercase" href="/">
+                            <FontAwesomeIcon icon={faKeyboard} /> Writing
+                        </NavItem>
+                        <NavItem className="ahamono to-uppercase" href="/">
+                            <FontAwesomeIcon icon={faGamepad} /> Gaming
+                        </NavItem>
+                        <NavItem className="ahamono to-uppercase" href="/">
+                            <FontAwesomeIcon icon={faBook} /> Meta
+                        </NavItem>
+                        <NavItem className="ahamono to-uppercase" href="/">
+                            <FontAwesomeIcon icon={faEnvelope} /> Contact
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
     }
